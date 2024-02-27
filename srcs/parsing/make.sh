11fe03aedc926886e4f31938f75d988607152003
welhox <casimir.lundberg@gmail.com>
2024-02-27 23:55:06 +0200

    skeleton frame of parsing

---
+++ srcs/parsing/make.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+cc parse.c pipex_split.c pipex_split_helper.c ../../libft/libft.a
--- srcs/parsing/parse.c
+++ srcs/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: clundber <clundber@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 11:44:03 by clundber          #+#    #+#             */
-/*   Updated: 2024/02/26 16:50:39 by clundber         ###   ########.fr       */
+/*   Updated: 2024/02/27 21:00:36 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,54 +20,30 @@
 	
 } */
 
-char	*get_token(char *argv)
+char	*env_variable(char *str, char **envp)
 
 {
-	static int	start;
-	int			end;
 
-	if (!start)
-		start = 0;
-	while (argv[start + end])
-	{
-		while (argv[start] == ' ' && argv[start]) // add all other whitespace
-			start++;
-		end = start;
-		while (argv[end] != ' ' && argv[end]) // add all other whitespace
-			end++;
-		return (ft_substr(argv, start, end - start));
-	}
-	return (0);
+	
 }
 
-char	**array_copy(char **array)
+void	var_substitution(char **array[], char *envp[])
 
 {
-	char	**new_array;
-	int		i;
+	int	i;
 
-	i = 0;
-	new_array = NULL;
-	while (array[i])
-		i++;
-	new_array = malloc(sizeof(char *) * (i +1));
-	if (!new_array)
-		return (0);
 	i = 0;
 	while (array[i])
 	{
-		new_array[i] = ft_strdup(array[i]);
-		if (!new_array[i])
+		if (ft_strrchr(array[i], '$'))
 		{
-			ft_arrfree(new_array);
-			return (0);
+			
 		}
+
 		i++;
 	}
-	new_array[i] = NULL;
-	return (new_array);
-}
 
+}
 void	error_func(char *str)
 
 {
@@ -75,42 +51,20 @@ void	error_func(char *str)
 	exit (1);
 }
 
-char	**lexer(char *argv)
+char	**lexer(char *argv, char **envp)
 
 {
-	char	**temp_array;
-	char	**temp_array2;
-	int		i;
-	int		array_size;
+	char **array;
 
-	array_size = 1;
-	i = 0;
-	temp_array2 = NULL;
-	temp_array = NULL;
-	while (1)
-	{
-		temp_array = malloc(sizeof(char *) * ((array_size * 20) +1));
-		if (!temp_array)
-			error_func("malloc failure\n");
-		if (temp_array2)
-			temp_array = array_copy(temp_array2);
-		if (!temp_array)
-			error_func("malloc failure\n");
-		while (i < 20 * array_size)
-		{
-			temp_array[i] = get_token(argv);
-			if (temp_array[i] == NULL)
-				break ;
-			i++;
-		}
-		array_size++;
-		temp_array2 = array_copy(temp_array);
-		ft_arrfree (temp_array);
-	}
-	return (temp_array);
+	array = NULL;
+	array = ppx_split(argv, ' ');
+	if (!array)
+		exit(1);
+	var_substitution(&array, envp);
+	return (array);
 }
 
-int	main(int argc, char *argv[])
+int	main(int argc, char *argv[], char *envp[])
 
 {
 	t_cmd	*cmd_head;
@@ -118,7 +72,9 @@ int	main(int argc, char *argv[])
 
 	array = NULL;
 	cmd_head = NULL;
-	array = lexer(argv[1]);
+	if (argc < 2 || !argv[1][0])
+		return(0);
+	array = lexer(argv[1], envp);
 
 	int	i = 0;
 	while (array[i])
